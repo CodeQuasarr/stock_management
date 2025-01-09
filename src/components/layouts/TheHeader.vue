6d0847876e39d226aa1b616359c540bf3f3179a2
Samuel ANYARONKE <dsanyaronke@gmail.com>
2025-01-09 01:29:45 +0100

    V1

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -23,23 +23,23 @@ const emit = defineEmits(["update:modelValue"]);
                         </svg>
                     </button>
                 </div>
-                <div class="flex-1 max-w-lg mx-8">
-                    <div class="relative">
-                        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                        </svg>
+<!--                <div class="flex-1 max-w-lg mx-8">-->
+<!--                    <div class="relative">-->
+<!--                        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400">-->
+<!--                            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />-->
+<!--                        </svg>-->
 
-                        <input
-                            type="text"
-                            placeholder="Recherche"
-                            class="w-full pl-10 pr-4 py-2 bg-gray-50 border border-gray-200 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
-                        />
-                    </div>
-                </div>
-                <div class="flex items-center space-x-4">
-                    <NotificationsDropdown />
-                    <UserDropdown />
-                </div>
+<!--                        <input-->
+<!--                            type="text"-->
+<!--                            placeholder="Recherche"-->
+<!--                            class="w-full pl-10 pr-4 py-2 bg-gray-50 border border-gray-200 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"-->
+<!--                        />-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="flex items-center space-x-4">-->
+<!--                    <NotificationsDropdown />-->
+<!--                    <UserDropdown />-->
+<!--                </div>-->
             </div>
         </div>
     </header>
